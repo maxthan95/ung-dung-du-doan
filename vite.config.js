5e9fa4cb5b4585bb42f8656bcafe05eb25f96c59
maxthan95 <maxsetup17@gmail.com>
2025-08-28 00:47:32 +0700

    Configure for GitHub Pages and re-add dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Thêm dòng này để chỉ định đường dẫn cơ sở cho dự án
+  // Thay 'ung-dung-du-doan' bằng tên repository của bạn nếu khác
+  base: '/ung-dung-du-doan/', 
   plugins: [react()],
 })
